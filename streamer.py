023691fffa4fb349502b26faeeaab95ec7fa8683
Paul Bruffett <paulbruffett@protonmail.com>
2023-08-26 08:51:58 -0700

    updated gen

--- streamer.py
+++ streamer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from flask import Flask, render_template, Response
 import cv2 as cv
+import time
 
 app = Flask(__name__)
 
@@ -14,7 +15,11 @@ def gen():
     else:
         rval = False
     while rval:
-        return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n'
+               b'\r\n' + frame + b'\r\n')
+        time.sleep(0.04) # my Firefox needs some time to display image / Chrome displays image without it 
+                         # 0.04s = 40ms = 25 frames per second
 
 @app.route('/')
 def index():
